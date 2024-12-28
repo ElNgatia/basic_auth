9b62d7d8981c4089352587a602c8279df115d0f9
ElNgatia <ngatiadyte@gmail.com>
2024-12-28 16:09:39 +0300

    add user login check and redirect to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:basic_auth/home_screen.dart';
 import 'package:basic_auth/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Check if the user is logged in
+  const secureStorage = FlutterSecureStorage();
+  final String? jwt = await secureStorage.read(key: 'jwt');
+
+  final bool isLoggedIn = jwt != null;
+  if (isLoggedIn) {
+    print('User is logged in');
+  } else {
+    print('User is not logged in');
+  }
   runApp(MaterialApp(
-    home: LoginScreen(),
+    home: isLoggedIn ? const HomeScreen() : LoginScreen(),
   ));
 }
